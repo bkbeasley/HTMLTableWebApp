fa16e84eec184f527c2d41bee1184670a279a1ed
bkbeasley <bbeasley344@gmail.com>
2017-03-22 01:28:14 -0500

    clean up code

--- script.js
+++ script.js
@@ -2,40 +2,50 @@ window.onload = function() {
   createTable(table_data);	
 };
 
-
+// This function dynamically generates and inserts an HTML table into the DOM
 function createTable(data) {
 	// Make the table responsive
 	var tableDiv = document.getElementById('tableDiv');
 	tableDiv.style.overflowX = "auto";
 
+
+	// Create a table and add styling to it
 	var table = document.createElement('TABLE');
 	table.border = "2px";
 	table.style.width = "100%";
 	table.style.borderCollapse = "collapse";
 
+
+	// Add a header to the table
 	var tableHeader = document.createElement('THEAD');
 	table.appendChild(tableHeader);
-	var tr = document.createElement('TR');
-	var fn = document.createElement('TH');
-	var ln = document.createElement('TH');
-	var hl = document.createElement('TH');
 
+
+	// Create a header row and header cells
+	var headerRow = document.createElement('TR');
+	var firstNameHeader = document.createElement('TH');
+	var lastNameHeader = document.createElement('TH');
+	var homeHeader = document.createElement('TH');
+
+	// Populate the header row
 	tableHeader.appendChild(tr);
-	fn.innerHTML = "First Name";
-	ln.innerHTML = "Last Name";
-	hl.innerHTML = "Home";
+	firstNameHeader.innerHTML = "First Name";
+	lastNameHeader.innerHTML = "Last Name";
+	homeHeader.innerHTML = "Home";
 
-	tr.appendChild(fn);
-	tr.appendChild(ln);
-	tr.appendChild(hl);
+	headerRow.appendChild(firstNameHeader);
+	headerRow.appendChild(lastNameHeader);
+	headerRow.appendChild(homeHeader);
 
 
+	// Create a table body
 	var tableBody = document.createElement('TBODY');
-
 	table.appendChild(tableBody);
 
+	// Store the size of the sample data
 	var dataSize = data.length;
 
+	// Create a stylized 3x3 table using data from the array passed
 	for(var i = 0; i < dataSize; i++) {
 		var tableRow = document.createElement('TR');
 		tableBody.appendChild(tableRow);
@@ -60,7 +70,7 @@ function createTable(data) {
 };
 
 
-
+// Data structure containing sample data for the dynamic table
 var table_data = [ { first_name : 'Rose',
                      last_name  : 'Tyler',
                      home       : 'Earth' },
