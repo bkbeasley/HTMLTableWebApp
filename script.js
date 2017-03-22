3138fe0994de7da03fbfe6f951544caddb29324f
bkbeasley <bbeasley344@gmail.com>
2017-03-22 01:06:00 -0500

    add styling to the table and make it responsive

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 window.onload = function() {
   createTable(table_data);	
-
-
 };
 
 
 function createTable(data) {
+	// Make the table responsive
 	var tableDiv = document.getElementById('tableDiv');
+	tableDiv.style.overflowX = "auto";
 
 	var table = document.createElement('TABLE');
+	table.border = "2px";
+	table.style.width = "100%";
+	table.style.borderCollapse = "collapse";
+
+	var tableHeader = document.createElement('THEAD');
+	table.appendChild(tableHeader);
+	var tr = document.createElement('TR');
+	var fn = document.createElement('TH');
+	var ln = document.createElement('TH');
+	var hl = document.createElement('TH');
+
+	tableHeader.appendChild(tr);
+	fn.innerHTML = "First Name";
+	ln.innerHTML = "Last Name";
+	hl.innerHTML = "Home";
+
+	tr.appendChild(fn);
+	tr.appendChild(ln);
+	tr.appendChild(hl);
+
+
 	var tableBody = document.createElement('TBODY');
 
 	table.appendChild(tableBody);
@@ -20,14 +41,17 @@ function createTable(data) {
 		tableBody.appendChild(tableRow);
 
 		var firstName = document.createElement('TD');
+		firstName.style.padding = "5px";
 		firstName.innerHTML = data[i].first_name;
 		tableRow.appendChild(firstName);
 
 		var lastName = document.createElement('TD');
+		lastName.style.padding = "5px";
 		lastName.innerHTML = data[i].last_name;
 		tableRow.appendChild(lastName);
 
 		var homeLocation = document.createElement('TD');
+		homeLocation.style.padding = "5px";
 		homeLocation.innerHTML = data[i].home;
 		tableRow.appendChild(homeLocation);
 	}
